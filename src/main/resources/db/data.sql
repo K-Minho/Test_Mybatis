b4575623345fa6b7c83a0141a2fedc67dfd592a8
K-Minho <alsgh9979@naver.com>
2023-01-26 11:17:11 +0900

    더미데이터 생성

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,16 @@
+insert into user_tb(username, password, email, created_at) values('Keter', '1234', 'keter@email.com', now());
+insert into user_tb(username, password, email, created_at) values('Hokhmah', '1234', 'hokhmah@email.com', now());
+insert into user_tb(username, password, email, created_at) values('Binah', '1234', 'binah@email.com', now());
+insert into user_tb(username, password, email, created_at) values('Chesed', '1234', 'Chesed@email.com', now());
+insert into user_tb(username, password, email, created_at) values('Gebura', '1234', 'Gebura@email.com', now());
+insert into user_tb(username, password, email, created_at) values('Tipheret', '1234', 'Tipheret@email.com', now());
+insert into user_tb(username, password, email, created_at) values('Netzach', '1234', 'Netzach@email.com', now());
+insert into user_tb(username, password, email, created_at) values('Hod', '1234', 'Hod@email.com', now());
+insert into user_tb(username, password, email, created_at) values('Yesod', '1234', 'Yesod@email.com', now());
+insert into user_tb(username, password, email, created_at) values('Malkuth', '1234', 'Malkuth@email.com', now());
+
+insert into board_tb(title, userId, created_at) values("First", 1, now());
+insert into board_tb(title, userId, created_at) values("Second", 1, now());
+insert into board_tb(title, userId, created_at) values("Third", 1, now());
+insert into board_tb(title, userId, created_at) values("Fourth", 1, now());
+insert into board_tb(title, userId, created_at) values("Fifth", 1, now());
